82b814af76ff481d417e1bd76ae36150d043b7c6
Milo Schwartz <mschwartz10612@gmail.com>
2023-01-10 14:52:43 -0500

    added gross profit and shareholder equity endpoints

--- venv/parse.py
+++ venv/parse.py
@@ -20,6 +20,12 @@ def get_net_income(income_stmt):
         net_income[int(year['date'].split("-")[0])] = int(year['netIncome']) // 1000000 
     return net_income
 
+def get_gross_profit(income_stmt):
+    gross_profit = {}
+    for year in income_stmt:
+        gross_profit[int(year['date'].split("-")[0])] = int(year['grossProfit']) // 1000000 
+    return gross_profit
+
 # CASH FLOW STATEMENT
 def get_free_cash_flow(cash_flow_stmt):
     free_cash_flow = {}
@@ -28,11 +34,11 @@ def get_free_cash_flow(cash_flow_stmt):
     return free_cash_flow
 
 # BALANCE SHEET
-def get_long_term_debt(balance_sheet):
-    long_term_debt = {}
+def get_debt(balance_sheet):
+    debt = {}
     for year in balance_sheet:
-        long_term_debt[int(year['date'].split("-")[0])] = int(year['longTermDebt']) // 1000000  
-    return long_term_debt
+        debt[int(year['date'].split("-")[0])] = int(year['totalDebt']) // 1000000  
+    return debt
 
 def get_cash(balance_sheet):
     cash = {}
@@ -40,6 +46,13 @@ def get_cash(balance_sheet):
         cash[int(year['date'].split("-")[0])] = int(year['cashAndCashEquivalents']) // 1000000  
     return cash
 
+def get_stockholder_equity(balance_sheet):
+    stockholder_equity = {}
+    for year in balance_sheet:
+        stockholder_equity[int(year['date'].split("-")[0])] = int(year['totalStockholdersEquity']) // 1000000  
+    return stockholder_equity
+
+# OTHER
 def get_profile(profile_sheet):
     profile = {}
     for key,value in profile_sheet[0].items():
@@ -49,7 +62,8 @@ def get_profile(profile_sheet):
 def get_ratios(ratios_sheet):
     ratios = {}
     for key,value in ratios_sheet[0].items():
-        ratios[key] = round(value, 2)
+        #ratios[key] = round(value, 2)
+        pass
     return ratios
 
 def get_financials(ticker):
@@ -60,8 +74,10 @@ def get_financials(ticker):
     financials['ebitda'] = get_ebitda(income_stmt)
     financials['revenue'] = get_revenue(income_stmt)
     financials['freeCashFlow'] = get_free_cash_flow(cash_flow_stmt)
-    financials['longTermDebt'] = get_long_term_debt(balance_sheet)
+    financials['totalDebt'] = get_debt(balance_sheet)
     financials['netIncome'] = get_net_income(income_stmt)
+    financials['totalStockholdersEquity'] = get_stockholder_equity(balance_sheet)
+    financials['grossProfit'] = get_gross_profit(income_stmt)
     financials['cash'] = get_cash(balance_sheet)
     with open("financials.json", "w") as f:
         f.write(json.dumps(financials, indent=4))
